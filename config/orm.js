62a854060f11678738aeaa3ba6ea4772b785b694
drthisguy <page.c.tyler@gmail.com>
2020-02-27 10:56:54 -0500

    select all method created

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,12 @@
         const connection = require('./connection');
 
+const orm = {
+    selectAll: function(table, cb) {
+            connection.query(`SELECT * FROM ${table};`, (err, res) => {
+                    if (err) throw err;
+
+                    cb(res);
+            });
+    },
+    
+}
