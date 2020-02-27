b30098f5aa2909d7a4c24fd7ba80c4e6d190bdb1
drthisguy <page.c.tyler@gmail.com>
2020-02-26 23:01:29 -0500

    MySQL database set up and running with schema and seeds file.

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Hamburger'), ('Cheeseburger'), ('Turkey Burger'), ('Mushroom Burger'), ('Salmon Burger');
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Cheesburger', true), ('Veggie Burger', true), ('Bacon Cheeseburger', true);
