10c6e153e351a511adefd6ab296f08a94e19d3e8
drthisguy <page.c.tyler@gmail.com>
2020-02-27 15:51:17 -0500

    update method added to orm.js with another helper function for building sql query strings.

--- config/orm.js
+++ config/orm.js
@@ -6,6 +6,17 @@ function getQuestionMarks(arr) {
 
  }
 
+ function sqlizor(obj) {
+   const arr = [];
+
+   Object.keys(obj).forEach( key => {
+           let val = obj[key].toString();
+           val = val.indexOf(' ') !== -1 ? `'${val}'` : val;
+           arr.push(`${key}=${val}`)
+   });
+   return arr.toString();
+  }
+
 const orm = {
     selectAll: function(table, cb) {
             connection.query(`SELECT * FROM ${table};`, (err, res) => {
@@ -16,7 +27,7 @@ const orm = {
     },
     
     insterOne: function(table, cols, vals, cb) {
-            let queryString = `INSERT INTO ${table} (${cols.toString()}) 
+            const queryString = `INSERT INTO ${table} (${cols.toString()}) 
                 VALUES (${getQuestionMarks(vals)}) `
             
              console.log(queryString);
@@ -26,6 +37,17 @@ const orm = {
 
                      cb(res);
              });
+    },
+
+    updateOne: function(table, elements, condition, cb) {
+            const queryString =  `UPDATE ${table} SET ${sqlizor(elements)} WHERE ${condition}`; 
+
+                console.log(queryString);
+            connection.query(querySting, (err, res) => {
+                    if (err) throw err;
+
+                    cb(res);
+            })
     }
 }
 module.exports = orm;
