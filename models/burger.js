34c33fa4fc72a2b8310758637d3867d29e958531
drthisguy <page.c.tyler@gmail.com>
2020-02-28 18:28:59 -0500

    created a burger.js file to function as a model for to db handleing.

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+            const orm = require('../config/orm'),
+
+
+const burger = {
+    all: function(cb) {
+        orm.selectAll('burgers', (res) => {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.insterOne('burgers', cols, vals, (res) => {
+            cb(res);
+        });
+    },
+
+    devoure: function(elements, condition, cb) {
+        orm.updateOne('burgers', elements, condition,  (res) => {
+            cb(res);
+        });
+    }
+}
+module.exports = burger;
