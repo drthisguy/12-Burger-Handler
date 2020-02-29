3e7f43740e3e9d85d1e2145710e03d9c9f1f9208
drthisguy <page.c.tyler@gmail.com>
2020-02-28 23:58:59 -0500

    server.js done.  Set the port up to use handlebars and the controller.

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-            const express = require('express');
+            const exphbs = require('express-handlebars'),
+                express = require('express'),
+                routes = require('./controllers/burgers_controller'), 
+
+
+app = express(),
+
+PORT = process.env.PORT  ||  8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(routes);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost: ${PORT}`);
+})
